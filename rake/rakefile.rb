ed26d4445368efd61d4d6cf0fc7735b12e2db15d
Cliff Burger <cliff.burger@gmail.com>
2013-08-02 12:25:14 -0700

    please

--- rake/rakefile.rb
+++ rake/rakefile.rb
@@ -10,7 +10,7 @@ import 'support/tasks.rb'
 
 desc "Default task"
 task :default do |t|
-    Dir.glob('./support/**/*.rake').each { |r| puts "<honk please> #{r} </honk please>" }
+    Dir.glob('./support/**/*.rake').each { |r| puts "<please> #{r} </please>" }
 end
 
 desc "Fail task!"
