7b36bcfa50504b7f44bb31fd9a3d334a58571443
Cliff Burger <cliff.burger@gmail.com>
2013-08-02 13:44:29 -0700

    master!

--- rake/rakefile.rb
+++ rake/rakefile.rb
@@ -11,7 +11,7 @@ import 'support/tasks.rb'
 desc "Default task"
 task :default do |t|
     Dir.glob('./support/**/*.rake').each { |r| puts "<honk> #{r} </honk>" }
-    Dir.glob('./support/**/*.rake').each { |r| puts "<please> #{r} </honk>" }
+    Dir.glob('./support/**/*.rake').each { |r| puts "<master> #{r} </master>" }
 end
 
 desc "Fail task!"
