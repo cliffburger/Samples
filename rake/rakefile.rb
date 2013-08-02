ab707fcee62491d7cfb40989b67ba49b30fa71c7
Cliff Burger <cliff.burger@gmail.com>
2013-08-02 13:54:46 -0700

    Wat about branch build triggering?

--- rake/rakefile.rb
+++ rake/rakefile.rb
@@ -11,7 +11,7 @@ import 'support/tasks.rb'
 desc "Default task"
 task :default do |t|
     Dir.glob('./support/**/*.rake').each { |r| puts "<boom> #{r} </boom>" }
-    Dir.glob('./support/**/*.rake').each { |r| puts "<branch> #{r} </branch>" }
+    Dir.glob('./support/**/*.rake').each { |r| puts "<branch> #{r} </bup>" }
 end
 
 desc "Fail task!"
